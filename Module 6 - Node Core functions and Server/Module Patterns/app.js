ef042f3878d27872c2d7cc0adfc3a730829196b8
abhisheksat <abhisheksatpute11@gmail.com>
2020-01-30 16:46:27 -0500

    Submit Module 6 - Module Patterns

---
+++ Module 6 - Node Core functions and Server/Module Patterns/app.js
@@ -0,0 +1,8 @@
+//  Network Based Application Development - ITIS 5166
+//  @Author - Abhishek Satpute | MS CS | Spring 2020
+
+var stuff = require('./stuff');
+
+console.log(stuff.counter(['Shaun', 'Crystal', 'Ryu']));
+console.log(stuff.adder(5,6));
+console.log(stuff.adder(stuff.pi, 5));
